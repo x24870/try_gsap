0502e80d849098f930b42cdd1a89970ca2461c45
pyk <x24870@gmail.com>
2020-10-02 13:40:13 +0800

    example 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+// example1
 gsap.to("h2.title", {duration: 1, opacity: 0.3});
-gsap.to(".box", {duration: 2, x: 300});
+// gsap.to(".box", {duration: 2, x: 300});
+gsap.to(".orange, .grey", {duration: 2, x: 300});
 gsap.to(".green", {duration: 3, rotation: 360, scale: 0.5});
+
+// example2
+CSSPlugin.defaultTransformPerspective = 400; 
+
+gsap.to(".green2", {duration: 3, rotationX: 360});
+gsap.to(".orange2", {duration: 3, rotationY: 360});
+gsap.to(".grey2", {duration: 3, x: 200, y: 100, scale: 2, skewX: 45, rotation: 180});
