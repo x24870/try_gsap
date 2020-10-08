bf1af4d111a4324376c2ceda0d5beef9631c60ba
pyk <x24870@gmail.com>
2020-10-08 15:15:36 +0800

    following cursor

--- following cursor/script.js
+++ following cursor/script.js
@@ -2,5 +2,8 @@ document.addEventListener('mousemove', (event) => {
   gsap.to('.box', {
     x: event.pageX,
     y: event.pageY,
-  })
+    stagger: 0.1,
+  });
+  console.log(event.pageX, event.pageY);
 });
+
