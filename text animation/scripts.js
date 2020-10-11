c1fa44b29406dbed40a36c60ffba8d9c4e71a20f
pyk <x24870@gmail.com>
2020-10-11 23:42:28 +0800

    finish text stagger animation

--- text animation/scripts.js
+++ text animation/scripts.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-  // let tl = gsap.timeline();
+  let tl = gsap.timeline();
 
-  // tl.fromTo("span", 1,
-  //   { bottom: -200, opacity: 0 },
-  //   { ease: "back.out", bottom: 0, opacity: 1, stagger: 0.2 }
-  // );
-
-  gsap.fromTo(".test span", 2,
-    { x: -100, opacity: 0 },
-    { x: 0, opacity: 1, stagger: 0.2 }
+  tl.fromTo(".title span", 1,
+    { bottom: -200, opacity: 0 },
+    { ease: "back.out", bottom: 0, opacity: 1, stagger: 0.2 }
   );
-  // gsap.from(".title sapn", 1, { bottom: -200, opacity: 0, ease: "back.out" })
+
 });
 
